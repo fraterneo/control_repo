cb67e1248bbbf9262fd0945e7096022f5f546ada
fraterneo <certad@gmail.com>
2020-07-22 09:40:20 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,3 +3,13 @@ node default {
 node 'master.puppet.vm' {
   include role::master_server
 }
+
+#Regex to match beginning with web
+node /^web/ {
+  include role::app_server
+}
+
+#Regex to match beginning with db
+node /^db/ {
+  include role::db_server
+}
